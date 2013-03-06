2f39304e80b135ef3dd0c2068a769e9b1e559fa1
imran esmail <imu_89@hotmail.com>
2013-03-06 21:43:18 +0000

    Added the functionality to hide and show patient's address and employer information.

--- App/viewmodels/patient/personalinformation.js
+++ App/viewmodels/patient/personalinformation.js
@@ -9,117 +9,28 @@ define(function(require) {
 	 **********************************************************************************************/
 	var system = require('durandal/system');			// System logger
 	var custom = require('durandal/customBindings');	// Custom bindings
-	var Backend = require('modules/patient');	// Module
-	
-	// Patient
-	function Patient(data) {
-		var self = this;
-		
-		self.practice   		  = ko.observable(data.practice_id);
-		self.id					  = ko.observable(data.id);
-		self.firstName  		  = ko.observable(data.first_name);
-		self.middleName 		  = ko.observable(data.middle_name);
-		self.lastName   		  = ko.observable(data.last_name);
-		self.alias			   	  = ko.observable(data.alias);
-		self.dob			   	  = ko.observable(data.dob);
-		self.idNumber  			  = ko.observable(data.id_number);
-		self.idType    			  = ko.observable(data.id_type);
-		self.physician  		  = ko.observable(data.physician_id);
-		self.address   			  = ko.observable(data.address);
-		self.city      			  = ko.observable(data.city);
-		self.state     			  = ko.observable(data.state);
-		self.zip       		 	  = ko.observable(data.zip);
-		self.province   		  = ko.observable(data.province);
-		self.country   			  = ko.observable(data.country);
-		self.phone   		      = ko.observable(data.phone);
-		self.phoneExt  			  = ko.observable(data.phone_ext);
-		self.mobile    		  	  = ko.observable(data.mobile);
-		self.gender     		  = ko.observable(data.gender);
-		self.familyHistoryType    = ko.observable(data.family_history_type);
-		self.familyHistoryComment = ko.observable(data.family_history_comment);
-		self.routineExamComment   = ko.observable(data.routine_exam_comment);
-		self.insuranceType        = ko.observable(data.insurance_type);
-		self.recordStatus   	  = ko.observable(data.record_status);
-		self.contactName 		  = ko.observable(data.contact_name);
-		self.contactPhone 		  = ko.observable(data.contact_phone);
-		self.contactMobile 	   	  = ko.observable(data.contact_mobile);
-		self.contactRelationship  = ko.observable(data.contact_relationship);
-		self.insuredType 		  = ko.observable('not insured');
-		
-		
-		// This will return the name in the following format: Last, First
-		self.lastFirstName = ko.computed(function() {
-			return self.lastName() + ", " + self.firstName();
-		});
-	}
-	// Insurance
-	function Insurance(data) {
-		var self = this;
-		
-		self.patientId   			= ko.observable(data.patient_id);
-		self.insuredType 			= ko.observable(data.type);
-		self.group       			= ko.observable(data.group_number);
-		self.policy      			= ko.observable(data.policy_number);
-		self.companyName 			= ko.observable(data.company_name);
-		self.plan        			= ko.observable(data.plan);
-		self.effectiveDate 			= ko.observable(data.effective_date);
-		self.outOfPocket 			= ko.observable(data.out_of_pocket);
-		self.metOutOfPocket 		= ko.observable(data.met_out_of_pocket);
-		self.remainingOutOfPocket 	= ko.observable(data.remaining_out_of_pocket);
-		self.deductible				= ko.observable(data.remaining_out_of_pocket);
-		self.metDeductible			= ko.observable(data.met_detuctible);
-		self.remainingDeductible	= ko.observable(data.remaining_deductible);
-		self.patientPortion			= ko.observable(data.patient_portion);
-		self.insurancePortion		= ko.observable(data.insurance_portion);
-		self.referralRequired		= ko.observable(data.referralRequired);
-		self.existingClause			= ko.observable(data.existing_clause);
-		self.copayment				= ko.observable(data.copayment);
-		self.verification			= ko.observable(data.verification);
-		self.verificationDate		= ko.observable(data.verification_datatime);
-		self.confirmationNumber		= ko.observable(data.confirmation_number);
-		self.contactName			= ko.observable(data.contact_name);
-		self.contactPhone			= ko.observable(data.contact_phone);
-		self.contactExt				= ko.observable(data.contact_phone_ext);
-		self.otherName				= ko.observable(data.other_name);
-	};
-	
-	// Guarantor
-	function Guarantor(data) {
-		var self = this;
-		
-		self.patientId   		= ko.observable(data.patient_id);
-		self.sameAsPatient		= ko.observable(data.same_as_patient);
-		self.name				= ko.observable(data.name);
-		self.relationship		= ko.observable(data.relationship);
-		self.idNumber			= ko.observable(data.id_number);
-		self.idType				= ko.observable(data.id_type);
-		self.dob				= ko.observable(data.date_of_birth);
-		self.address			= ko.observable(data.address);
-		self.city				= ko.observable(data.city);
-		self.state				= ko.observable(data.state);
-		self.zip				= ko.observable(data.zip);
-		self.province			= ko.observable(data.province);
-		self.country			= ko.observable(data.country);
-		self.employer			= ko.observable(data.employer);
-		self.employerPhone		= ko.observable(data.employer_phone);
-	};
+	//var Backend = require('modules/moduleTemplate');	// Module
 	
 	/*********************************************************************************************** 
 	 * KO Observables
 	 **********************************************************************************************/
-	var insuredPerson 		= ko.observable();
-	var primaryInsurance 	= ko.observable();
-	var secondaryInsurance  = ko.observable();
-	var otherInsurance 		= ko.observable();
-	var patientId 			= ko.observable();
-	var patient				= ko.observable();
-	var guarantor 			= ko.observable();
+	// var observable = ko.observable('');
+	// var observableArray = ko.observableArray([]);
+	var optionNumber = ko.observable(); 
 
 	/*********************************************************************************************** 
 	 * KO Computed Functions
 	 **********************************************************************************************/
 	 // var computedFunction = ko.computed(function() {});
-
+	 var selectedOption = ko.computed(function(){ 
+	 	if(optionNumber() == '3'){ 
+	 		return true;    
+	 	}
+	 	else 
+	 	{ 
+	 		return false; 
+	 	} 
+	 }); 
 	/*********************************************************************************************** 
 	 * ViewModel
 	 *
@@ -130,91 +41,37 @@ define(function(require) {
 		/******************************************************************************************* 
 		 * Attributes
 		 *******************************************************************************************/
-		patientId: patientId,
-		patient: patient,
-		insuredPerson: insuredPerson,
-		primaryInsurance: primaryInsurance,
-		secondaryInsurance: secondaryInsurance,
-		otherInsurance: otherInsurance,
-		guarantor: guarantor,
+		 optionNumber: optionNumber,
+		
 		/******************************************************************************************* 
 		 * Methods
 		 *******************************************************************************************/
 		// This allow manipulation of the DOM
 		viewAttached: function() {
+			$('#personalTab a').click(function(e) {
+				e.preventDefault();      
+  				$(this).tab('show');
+			});  
+			
 			// Resize tree and content pane
-			$('.tab-pane').height(parseInt($('.contentPane').height()) - 62);
+			$('.outerPane').height(parseInt($('.contentPane').height()) - 62);
 			$(window).resize(function() {
-				$('.tab-pane').height(parseInt($('.contentPane').height()) - 62);
+				$('.outerPane').height(parseInt($('.contentPane').height()) - 62);
 			});
+			
+			
 		},
 		// Loads when view is loaded
 		activate: function(data) {
-			var self = this;
+			// Code here
 			
-			// Patient ID
-			self.patientId(data.patientId);
-			
-			// Module object
-			var backend = new Backend();
-			/**************************************************************************************
-			 * Personal Information
-			 * 
-			 * Query the patient table to retrive peronsonal information for the patient.
-			 *************************************************************************************/
-			backend.getPatient(self.patientId()).success(function(data) {
-				if(data.length > 0) {
-					var p = new patient(data[0]);
-					self.patient(p);
-				}
-			});
-			
-			/**************************************************************************************
-			 * Gurantor Information
-			 * 
-			 * Query the insurance table to see if there exists an entry for a guarantor for
-			 * the patient. If there does, then set insuredPerson to other and populate guarantor
-			 * obsevable. If not, then set insuredPerson to self. Will check if insurance 
-			 * information exists in getInsurance.
-			 *************************************************************************************/ 
-			backend.getGuarantor(self.patientId()).success(function(data) {
-				if(data.length > 0) {
-					var g = new Guarantor(data[0]);
-					self.guarantor(g);
-					self.insuredPerson('other');
-				}
-				else {
-					self.insuredPerson('self');
-				}
-			});
-			
-			/**************************************************************************************
-			 * Insurance Information
-			 * 
-			 * If no insurance information is returned, set insuredPerson to not insured. Otherwise
-			 * for each type of insurance, set appropriate observable.
-			 *************************************************************************************/
-			return backend.getInsurance(self.patientId()).success(function(data) {
-				if(data.length > 0) {
-					for(var count = 0; count < data.length; count++) {
-						var i = new Insurance(data[count]);
-						switch(i.insuredType()) {
-	            			case 'primary':
-	            				self.primaryInsurance(i);
-	            				break;
-	            			case 'secondary':
-	            				self.secondaryInsurance(i);
-	            				break;
-	            			default:
-	            				self.otherInsurance(i);
-	            				break;
-	            		}
-	            	}
-				}
-				else {
-					self.insuredPerson('not insured')	
-				}
-			});
-		}
+			// If you add any asynchronous code, make sure you return it. If you need to add multiple
+			// asynchronous code, return the functions chained together. If you don't return them,
+			// then Durandal will not wait for them to finish before loading the rest of the page.
+			// There might be issues when updating observables.
+			// Ex:
+			// return .get().getJSON().post();
+		},
+		selectedOption: selectedOption,
 	};
 });
